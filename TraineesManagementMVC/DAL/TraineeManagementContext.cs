74ffdde51851a7c1efe307061064b7a832b3cb2d
ESSARRAJ <essarraj.fouad@gmail.com>
2018-02-20 21:44:52 +0100

    Add DbContext

---
+++ TraineesManagementMVC/DAL/TraineeManagementContext.cs
@@ -0,0 +1,17 @@
+﻿namespace TraineesManagementMVC.DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using TraineesManagementMVC.Models;
+
+    public class TraineeManagementContext : DbContext
+    {
+         public TraineeManagementContext()
+            : base("name=TraineeManagementContext")
+        {
+        }
+
+        public virtual DbSet<Group> Groups { get; set; }
+    }
+}
